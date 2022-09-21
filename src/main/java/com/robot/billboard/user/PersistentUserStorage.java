9a2c8a818e86f3d5c5503c28d7cd048d6dcf5a8c
Sergey Simenshteyn <simenshteyn@gmail.com>
2022-09-21 13:14:23 +0700

    Add checkstyle to project

--- src/main/java/com/robot/billboard/user/PersistentUserStorage.java
+++ src/main/java/com/robot/billboard/user/PersistentUserStorage.java
@@ -3,7 +3,6 @@ package com.robot.billboard.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Optional;
 
 @Component
